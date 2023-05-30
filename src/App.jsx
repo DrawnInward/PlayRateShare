3e160d644f87dc4a1d8763c81b87dad00472587a
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-05-30 16:24:46 +0100

    completed ticket 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Reviews from "./components/Reviews";
+import Review from "./components/Review";
 import Reviews from "./components/Reviews";
 import Nav from "./components/Nav";
 import Homepage from "./components/Home";
 
 function App() {
   return (
+    <BrowserRouter>
+      <>
+        <Routes>
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/review" element={<Review />} />
+        </Routes>
+      </>
+    </BrowserRouter>
     <BrowserRouter>
       <>
         <Nav />
