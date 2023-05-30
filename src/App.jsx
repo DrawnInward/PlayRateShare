b7d74df9465c3da4f5a897ecd8b35a371dfc1294
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-05-30 14:22:01 +0100

    added navbar etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Reviews from "./components/Reviews";
+import Nav from "./components/Nav";
+import Homepage from "./components/Home";
 
 function App() {
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <BrowserRouter>
+      <>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/reviews" element={<Reviews />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
