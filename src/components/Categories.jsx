6fb268932f67e8f05e2b06a75a76f3b1201f1fe9
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-07-20 16:18:31 +0100

    styled catergories with grid

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -20,7 +20,7 @@ const Categories = () => {
   return (
     <ul className="categoryList">
       {categories.map((category) => (
-        <li key={category.slug}>
+        <li key={category.slug} className={category.slug}>
           <Link to={`/reviews?category=${category.slug}`}>
             <h2>{category.slug}</h2>
             <h3>{category.description}</h3>
